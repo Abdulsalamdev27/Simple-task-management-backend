3630bd839a594d8f0d3f7c8f67e4549074c4d5cb
abdulsalamdev27 <abubakaabdulsalam691@gmail.com>
2026-02-19 06:32:45 -0800

    feat: add secure sign-up with password hashing and validation

--- src/server.js
+++ src/server.js
@@ -2,14 +2,25 @@ import express from "express";
 import "dotenv/config";
 
 
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js"
 
 
 
 const app = express();
+app.use(express.json())
 
 const PORT = process.env.PORT;
 
+
+
+app.use("/api/auth", authRoutes)
+
+
+app.use("/api",(req, res)=>{
+    res.send("Api is running")
+
+});
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`)
